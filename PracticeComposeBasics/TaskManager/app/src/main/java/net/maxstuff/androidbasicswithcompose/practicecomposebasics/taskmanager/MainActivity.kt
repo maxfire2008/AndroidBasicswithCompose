761f936354f107fcfffd191a9ada52b96c6ec5cd
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-08-23 13:24:22 +1000

    extract string resources

--- PracticeComposeBasics/TaskManager/app/src/main/java/net/maxstuff/androidbasicswithcompose/practicecomposebasics/taskmanager/MainActivity.kt
+++ PracticeComposeBasics/TaskManager/app/src/main/java/net/maxstuff/androidbasicswithcompose/practicecomposebasics/taskmanager/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,7 +52,7 @@ fun TasksCompleted(modifier: Modifier = Modifier) {
             modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         )
         Text(
-            text = "All tasks completed", fontWeight = FontWeight.Bold, modifier = Modifier
+            text = stringResource(R.string.all_tasks_completed), fontWeight = FontWeight.Bold, modifier = Modifier
                 .padding(
                     top = 24.dp,
                     bottom = 8.dp,
@@ -59,7 +60,7 @@ fun TasksCompleted(modifier: Modifier = Modifier) {
                 .align(alignment = Alignment.CenterHorizontally)
         )
         Text(
-            text = "Nice work!",
+            text = stringResource(R.string.nice_work),
             fontSize = 16.sp,
             modifier = Modifier
                 .align(alignment = Alignment.CenterHorizontally)
