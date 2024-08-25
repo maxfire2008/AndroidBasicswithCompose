5aef423398878cf7afdb2c147217b62e54e9a103
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-08-26 09:18:40 +1000

    complete quadrants

--- PracticeComposeBasics/ComposeQuadrant/app/src/main/java/net/maxstuff/androidbasicswithcompose/practicecomposebasics/composequadrant/MainActivity.kt
+++ PracticeComposeBasics/ComposeQuadrant/app/src/main/java/net/maxstuff/androidbasicswithcompose/practicecomposebasics/composequadrant/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,8 +15,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.maxstuff.androidbasicswithcompose.practicecomposebasics.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,42 +43,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Quadrant(title: String, description: String, modifier: Modifier = Modifier) {
-    Text(
-        title
-    )
+fun Quadrant(title: String, description: String, color: Color, modifier: Modifier = Modifier) {
+    Box(modifier = modifier.background(color = color)) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                title,
+                textAlign = TextAlign.Justify,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                description,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+
+    }
 }
 
 @Composable
 fun Quadrants(modifier: Modifier = Modifier) {
-    Row(
+    Column(
         modifier = modifier.fillMaxWidth()
     ) {
-        Column (
+        Row(
             modifier = Modifier.weight(1f)
         ) {
             Quadrant(
-                title = "test",
-                description = "hellosafd",
-                modifier = Modifier.weight(1f)
+                title = stringResource(R.string.quadrant_1_title),
+                description = stringResource(R.string.quadrant_1_description),
+                color = colorResource(R.color.quadrant_1),
+                modifier = Modifier.weight(1f),
             )
             Quadrant(
-                title = "test",
-                description = "hellosafd",
-                modifier = Modifier.weight(1f)
+                title = stringResource(R.string.quadrant_2_title),
+                description = stringResource(R.string.quadrant_2_description),
+                color = colorResource(R.color.quadrant_2),
+                modifier = Modifier.weight(1f),
             )
         }
-        Column (
+        Row(
             modifier = Modifier.weight(1f)
         ) {
             Quadrant(
-                title = "test",
-                description = "hellosafd",
+                title = stringResource(R.string.quadrant_3_title),
+                description = stringResource(R.string.quadrant_3_description),
+                color = colorResource(R.color.quadrant_3),
                 modifier = Modifier.weight(1f)
             )
             Quadrant(
-                title = "test",
-                description = "hellosafd",
+                title = stringResource(R.string.quadrant_4_title),
+                description = stringResource(R.string.quadrant_4_description),
+                color = colorResource(R.color.quadrant_4),
                 modifier = Modifier.weight(1f)
             )
         }
