5cb912d7e443334aa7435de489c998ffc6e31c72
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-08-26 13:25:38 +1000

    add profile picture and alignment

--- BusinessCard/app/src/main/java/net/maxstuff/androidbasicswithcompose/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/net/maxstuff/androidbasicswithcompose/businesscard/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import net.maxstuff.androidbasicswithcompose.businesscard.ui.theme.BusinessCardTheme
@@ -33,18 +38,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard(modifier: Modifier = Modifier) {
-    Column(
-
+    val image = painterResource(R.drawable.pfp)
+    Box(
+        modifier = modifier.fillMaxSize()
     ) {
-        Text(
-            "Full Name",
-            fontSize = 24.sp,
-        )
-    }
-    Column(
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+        ) {
+            Image(
+                painter=image,
+                contentDescription="Max's profile picture",
+                alignment = Alignment.Center,
+            )
+            Text(
+                "Full Name",
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+            )
+        }
+        Column(
 
-    ) {
+        ) {
 
+        }
     }
 }
 
