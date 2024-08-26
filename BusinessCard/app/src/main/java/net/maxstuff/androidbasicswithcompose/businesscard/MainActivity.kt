2272022cf10f37c8c143588e938d6359360b12ae
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-08-27 08:55:53 +1000

    add padding and attempt to add icon

--- BusinessCard/app/src/main/java/net/maxstuff/androidbasicswithcompose/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/net/maxstuff/androidbasicswithcompose/businesscard/MainActivity.kt
@@ -7,16 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.maxstuff.androidbasicswithcompose.businesscard.ui.theme.BusinessCardTheme
 
@@ -47,20 +58,37 @@ fun BusinessCard(modifier: Modifier = Modifier) {
                 .align(Alignment.Center)
         ) {
             Image(
-                painter=image,
-                contentDescription="Max's profile picture",
+                painter = image,
+                contentDescription = "Max's profile picture",
                 alignment = Alignment.Center,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
             )
             Text(
                 "Full Name",
+                fontSize = 50.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top=20.dp, bottom=1.dp)
+                    .fillMaxWidth(),
+            )
+            Text(
+                "Title",
                 fontSize = 24.sp,
                 textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth(),
             )
         }
         Column(
-
+            modifier = Modifier
+                .align(Alignment.Center)
         ) {
-
+            Row {
+                Icon(Icons.Rounded.Menu, contentDescription = "Localized description")
+            }
         }
     }
 }
